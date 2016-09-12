b97ef2dc7f348d75c859691b12e4a161b9572d65
Adam Pallin <apallin@lyft.com>
2016-09-11 19:41:50 -0700

    Removing helpers reference in __init__

--- testworksappium/__init__.py
+++ testworksappium/__init__.py
@@ -4,4 +4,3 @@ from elements import Elements  # noqa
 import exceptions  # noqa
 from page import Page  # noqa
 from testcase import AppiumTestCase  # noqa
-from helpers import class_repr, class_string  # noqa
